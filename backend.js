24d58b315ace4791258d36e422530ff8bb2bbdc4
aayush <aayushpatidar047@gmail.com>
2025-08-19 16:02:09 +0530

    fix: added CORS preflight support

--- backend.js
+++ backend.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.options("*", cors()); 
 app.use(bodyParser.json());
 
 app.post("/api/contact", async (req, res) => {
